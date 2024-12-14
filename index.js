2728169ed7c81135566221ae94672cf31ff0d33d
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-12-14 20:38:43 +0530

    chanes done for video stream

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //video streaming route on the same server
 app.get('/video', (req, res) => {
-    const videoPath = "chaiya-chaiya.mp4";
-    const videoSize = fs.statSync("chaiya-chaiya.mp4").size;
-    console.log(videoSize);
-
     const range = req?.headers?.range;
     if(!range) return res.status(400).json({"message": "Requires Range header"});
+
+    const videoPath = "chaiya-chaiya.mp4";
+    const videoSize = fs.statSync("chaiya-chaiya.mp4").size;
+
+    //Parse Range for video
+    const CHUNK_SIZE = 10 ** 6; //1 MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize -1);
+    const contentLength = end - start + 1;
+
+    const headers =  {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
 })
 
 
