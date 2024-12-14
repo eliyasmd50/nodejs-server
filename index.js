84c9c023d1cc941ed18fd99dd7dc5eace38a9ab5
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-12-14 22:43:27 +0530

    vdeo stream from mongo DB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const { MongoClient, GridFSBucket } = require('mongodb');
+const url = 'mongodb://localhost:27017/';
+const client = new MongoClient(url);
+const db = client.db("video-stream");
+const bucket = new GridFSBucket(db);
 const app = express();
 
 //middleware in express
@@ -10,32 +14,57 @@ app.use(express.json());
 //to server static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// DB connect
+app.get('/init-mongo', async (req, res) => {
+    try {
+        await client.connect();
+        const videoUploadStream = bucket.openUploadStream('youtube');
+        const videoReadStream = fs.createReadStream('./chaiya-chaiya.mp4');
+        videoReadStream.pipe(videoUploadStream);
+        res.status(200).send("Done...");
+    } catch (error) {
+        console.log(error.message);
+        res.json(error);
+    } finally {
+        await client.close();
+    }
+})
+
 //video streaming route on the same server
-app.get('/video', (req, res) => {
+app.get('/mongo-video', async (req, res) => {
     const range = req?.headers?.range;
     if(!range) return res.status(400).json({"message": "Requires Range header"});
 
-    const videoPath = "chaiya-chaiya.mp4";
-    const videoSize = fs.statSync("chaiya-chaiya.mp4").size;
+    try {
+        await client.connect();
+        const video = await db.collection('fs.files').findOne({});
+        if(!video) {
+            res.status(404).send("No videos Uploaded");
+            return;
+        } 
+        //Taking video size from the mongo
+        const videoSize = video.length;
+        console.log(videoSize);
+        const start = Number(range.replace(/\D/g, ""));
+        const end =  videoSize -1;
+        const contentLength = end - start + 1;
 
-    //Parse Range for video
-    const CHUNK_SIZE = 10 ** 6; //1 MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize -1);
-    const contentLength = end - start + 1;
+        const headers =  {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            "Content-Type": "video/mp4"
+        };
+        // http headers for the partial content
+        res.writeHead(206, headers);
+        const downloadStream = bucket.openDownloadStreamByName('youtube', { start });
 
-    const headers =  {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4"
-    }
-    res.writeHead(206, headers);
-
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+        // streaming the video
+        downloadStream.pipe(res);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error.message);
+    } 
 })
-
-
 
 app.listen(3500, () => console.log(`Server running on the port 3500`));
